2dc3e4e61e50410559f11938213ad8f9e7da7d82
Celine86 <celine.obringer@live.fr>
2023-11-12 15:29:44 +0100

    Server initialisation

---
+++ app.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express'); 
+const helmet = require('helmet');
+const app = express();
+const path = require("path");
+
+app.use(helmet());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+}); 
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+/* 
+app.use("/images", express.static(path.join(__dirname, "images")));
+app.use('/api/users', require('./routes/user'));
+*/
+
+module.exports = app;
